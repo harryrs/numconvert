ba1de99d7056b1e25a53703933c4e05a0dd336ee
Harry Silverman <harryrs@umich.edu>
2019-03-02 13:31:05 -0500

    Clean up style and add comments
    Haven't had enough time to improve functionality, but I did get a
    quick chance to make the program look at least a little nicer from
    a style perspective

--- convert.c
+++ convert.c
@@ -20,46 +20,35 @@
 int main(int argc, char* argv[]) {
 
     if (argc != 2) {
-
         printf("usage: ./convert [number]\n");
-	exit(1);
+	    exit(1);
     }
     int num = atoi(argv[1]);
     if (num < 0) {
-
         printf("program currently only supports conversion ");
         printf("of positive numbers to 32-bit binary\n");
         exit(1);
     }
-    int bits[32];
-    int bitsNeeded = 0;
+    int bits[32];        // The C programming language cannot support integers as big or bigger than
+    int bitsNeeded = 0;  // 2^31, so neither can this program
 
     while (num > 0) {
 
-        int one = 1;
-        if ((num & 1) == 1) {
-
-            bits[bitsNeeded] = 1;
-        }
-        else {
-
-            bits[bitsNeeded] = 0;
-        }
-        num = num / 2;
-        bitsNeeded = bitsNeeded + 1;
+        bits[bitsNeeded] = num & 1;   // get current bit and put in appropriate array index
+        num = num / 2;                // equivalent to num = num >> 1, or bit-shifting right by one
+        bitsNeeded = bitsNeeded + 1;  // update index
     }
     if (bitsNeeded == 0) {
 
         printf("%d", bitsNeeded);
     }
     else {
-
+        // printing from left-to-write for output readability
         for (int i = bitsNeeded - 1; i >= 0; i = i - 1) {
 
             printf("%d", bits[i]);
         }
     }
     printf("\n");
-
     return 0;
 }
