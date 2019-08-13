7ced3940a4a19c5ea669414bca1b57149faf8a6f
Harry Silverman <harryrs@umich.edu>
2019-08-13 10:42:50 -0400

    Add more comprehensive error checking for input
    This will be necessary for when I add support for converting from
    binary to decimal, hex to decimal, etc. I want this application to
    be as user-friendly as possible, which will already be a challenge
    due to my choice to make it a command-line application.

--- numconvert.c
+++ numconvert.c
@@ -17,12 +17,56 @@
 #include <stdio.h>
 #include <string.h>
 
+void decimal_to_binary(int num);
+void binary_to_decimal(int num);
+
+void decimal_to_hex(int num);
+void hex_to_decimal(int num);
+
+void hex_to_binary(char* num);
+void binary_to_hex(char* num);
+
 int main(int argc, char* argv[]) {
 
-    if (argc != 2) {
-        printf("usage: ./convert [number]\n");
+    if (argc < 2 || argc > 4 || argc == 3) {
+        printf("usage: ./convert <number> <convert_from> <convert_to>\n");
+        printf("Enter ./convert --help for more information\n");
 	    exit(1);
+
+    } else if (argc == 2) {
+
+        if (strcmp(argv[1], "--help") == 0) {
+            printf("./convert\n");
+            printf("<number>          The number you would like to convert");
+            printf("<convert_from>    Numeral system of <number> (-b for binary, -x for hex, -d for decimal)");
+            printf("<convert_to>      Numeral system to convert to (-b for binary, -x for hex, -d for decimal)");
+            exit(1);
+
+        } else {
+            printf("usage: ./convert <number> <convert_from> <convert_to>\n");
+            printf("Enter ./convert --help for more information\n");
+	        exit(1);
+        }
     }
+
+    if (!(strcmp(argv[2], "-b") == 0 || strcmp(argv[2], "-x") == 0 || strcmp(argv[2], "-d") == 0) ||
+        !(strcmp(argv[3], "-b") == 0 || strcmp(argv[3], "-x") == 0 || strcmp(argv[3], "-d") == 0)) {
+        
+        printf("Error: <convert_from> and <convert_to> arguments must be one of -b, -x, or -d\n");
+        printf("Enter ./convert --help for more information\n");
+        exit(1);
+    }
+
+    /*
+    if (strcmp(argv[2], "-b") == 0) {
+
+
+    } else if (strcmp(argv[2], "-x") == 0) {
+
+
+    } else {
+        
+    }*/
 
     int num = atoi(argv[1]);
     if (num < 0) {
@@ -30,7 +74,8 @@ int main(int argc, char* argv[]) {
         printf("of positive numbers to 32-bit binary\n");
         exit(1);
     }
-    unsigned int bit = 1 << 31; // C cannot support integers that are more than 32 bits
+
+    unsigned int bit = 1 << 31;
     int shifted = 31;
 
     // Optimization for the edge case where num is 0
